91d043095e529c3a8938f9de768da9357013dcd2
Tayyab <mtayyabmalik99@gmail.com>
2022-06-23 02:24:08 +0500

    updated

--- 03_config_vlan.py
+++ 03_config_vlan.py
@@ -4,7 +4,7 @@ import telnetlib
 
 from pyrsistent import b
 
-HOST = "10.10.10.6"
+HOST = "10.10.10.8"
 user = input("Enter your telnet username: \n")
 password = getpass.getpass()
 
@@ -17,26 +17,24 @@ if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode("ascii") +b"\n")
 
-tn.write(b"conf t \n")
+tn.write(b"vlan database \n")
 # Now we don't configure the enable password because we set the router as a privilege 15
 tn.write(b"vlan 2 \n")
 tn.write(b"name Python_VLAN_2 \n")
-tn.write(b"exit \n")
+# tn.write(b"exit \n")
 tn.write(b"vlan 3 \n")
 tn.write(b"name Python_VLAN_3 \n")
+# tn.write(b"exit \n")
+# tn.write(b"vlan 4 \n")
+# tn.write(b"name Python_VLAN_4 \n")
+# tn.write(b"exit \n")
+# tn.write(b"vlan 5 \n")
+# tn.write(b"vlan 5 name Python_VLAN_5 \n")
+# tn.write(b"exit \n")
+# tn.write(b"vlan 6 \n")
+# tn.write(b"vlan 6 name Python_VLAN_6 \n")
+# tn.write(b"exit \n")
 tn.write(b"exit \n")
-tn.write(b"vlan 4 \n")
-tn.write(b"name Python_VLAN_4 \n")
-tn.write(b"exit \n")
-tn.write(b"vlan 5 \n")
-tn.write(b"name Python_VLAN_5 \n")
-tn.write(b"exit \n")
-tn.write(b"vlan 6 \n")
-tn.write(b"name Python_VLAN_6 \n")
-tn.write(b"exit \n")
-tn.write(b"end \n")
 tn.write(b"exit \n")
 
 print(tn.read_all().decode("ascii"))
-
-tn.w
